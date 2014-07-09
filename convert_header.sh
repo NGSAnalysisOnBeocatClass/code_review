430b9546f4c188ea96bb255aa0eb6959a5b8b0c4
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-07-09 14:21:11 -0500

    Update convert_header.sh

--- convert_header.sh
+++ convert_header.sh
@@ -8,10 +8,10 @@
 #
 #########################################################################
 
-cd ~/class/fastq/header
+CD ~/class/fastq/header
 
 
 ## script converts to pre CASAVA 1.8 format for MIRA found from http://www.freelists.org/post/mira_talk/Metagenome-assembly,4 ##
-cat new_1.fastq | awk '{if (NR % 4 == 1) {split($1, arr, ":"); printf "%s_%s:%s:%s:%s:%s#0/%s\n", arr[1], arr[3], arr[4], arr[5], arr[6], arr[7], substr($2, 1, 1), $0} else if (NR % 4 == 3){print "+"} else {print $0} }' > ~/output/new_1_header.fastq
+cat new_1.fastq | awk '{if (NR % 4 == 1) {split($1, arr, ":"); printf "%s_%s:%s:%s:%s:%s#0/%s\n", arr[1], arr[3], arr[4], arr[5], arr[6], arr[7], substr($2, 1, 1), $0} else if (NR % 4 == 3){print "+"} else {print $0} }' > /homes/sheltonj/output/new_1_header.fastq
 
-cat new_2.fastq | awk '{if (NR % 4 == 1) {split($1, arr, ":"); printf "%s_%s:%s:%s:%s:%s#0/%s\n", arr[1], arr[3], arr[4], arr[5], arr[6], arr[7], substr($2, 1, 1), $0} else if (NR % 4 == 3){print "+"} else {print $0} }' > ~/output/new_2_header.fastq
+cat new_2.fastq | awk '{if (NR % 4 == 1) {split($1, arr, ":"); printf "%s_%s:%s:%s:%s:%s#0/%s\n", arr[1], arr[3], arr[4], arr[5], arr[6], arr[7], substr($2, 1, 1), $0} else if (NR % 4 == 3){print "+"} else {print $0} }' > /homes/sheltonj/output/new_2_header.fastq
